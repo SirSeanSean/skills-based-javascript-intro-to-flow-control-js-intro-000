f4291466549fd3500a63cb0641b3c7052fb5fa02
Sean Austin Rosenthal <sean.rosenthal@gloanyx.com>
2021-03-22 00:50:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -17,7 +17,18 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if(age <= 12)
+  {
+    return 'You are a kid'
+  }
+  else if (age > 19)
+    {
+      return 'You are a grownup'
+    }
+  else
+    {
+      return 'You are a teenager!'
+    }
 }
 
 function ternaryTeenager(age) {
